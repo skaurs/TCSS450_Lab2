abdc3d628b2ddba1f2265c3fe7bcecab57d91405
Sukhraj Kaur <skaur65@uw.edu>
2020-10-21 16:32:36 -0700

    Finished part 5 of tutorial: Add new Fragments to Navigate to

---
+++ app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/blog/BlogListFragment.java
@@ -0,0 +1,25 @@
+package edu.uw.tcss450.uiandnavigationlab.ui.blog;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.uiandnavigationlab.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlogListFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blog_list, container, false);
+    }
+}
