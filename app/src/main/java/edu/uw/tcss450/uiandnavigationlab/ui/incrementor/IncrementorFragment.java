29f50d828ae8479eb954be04a923408b4932fffa
Sukhraj Kaur <skaur65@uw.edu>
2020-10-21 22:35:33 -0700

    Finished part 12 Create the ViewModel

--- app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/incrementor/IncrementorFragment.java
+++ app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/incrementor/IncrementorFragment.java
@@ -2,13 +2,17 @@ package edu.uw.tcss450.uiandnavigationlab.ui.incrementor;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.uiandnavigationlab.R;
+import edu.uw.tcss450.uiandnavigationlab.databinding.FragmentIncrementorBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,5 +25,24 @@ public class IncrementorFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_incrementor, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FragmentIncrementorBinding binding = FragmentIncrementorBinding.bind(getView());
+
+        binding.textCount.setText("My Count is 0");
+
+        //obtain access to ViewModel
+        IncrementorViewModel model = new ViewModelProvider(this).get(IncrementorViewModel.class);
+
+        //add observer to the MutableLiveData - mCount
+        model.addCountObserver(getViewLifecycleOwner(), count ->
+                binding.textCount.setText("My Count is: " + model.getCount()));
+
+        //On button click increase MutableLiveData -mcount
+        binding.buttonIncrease.setOnClickListener(button -> model.increment());
     }
 }
